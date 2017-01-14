c33b1e617431b0e14079cf07d35af6e08fb99750
Lubomír Bláha <tritol@trilogic.cz>
2017-01-14 21:14:43 +0100

    Czech translation

---
+++ languages/cs.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Users Online
+ *
+ * Czech language file
+ *
+ * @package users_online
+ * @author tritol
+ * @copyright iionly 2014
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @website https://github.com/iionly
+ * @email iionly@gmx.de
+ */
+
+return array(
+	'users_online:online' => "Přihlášení uživatelé: ",
+	'users_online:noonline' => "nikdo",
+
+	'users_online:settings:user_listing_limit' => 'Kolik uživatelů má být maximálně zobrazeno? ',
+	'users_online:settings:display_option' => 'Kde se mají přihlášení uživatelé zobrazovat? ',
+	'users_online:settings:top' => 'v horní části stránky',
+	'users_online:settings:sidebar' => 'v postranní liště',
+	'users_online:settings:both' => 'v horní části stránky i v postranní liště'
+);
